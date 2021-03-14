8bf75cc35708b91ce1865beafc5a91072f8f867c
Rafael Santos Vieira <rafaelsvbr@gmail.com>
2021-03-14 14:51:53 -0300

    Add Context storage isolated and global in application

--- src/App.js
+++ src/App.js
@@ -1,20 +1,12 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 import MyContext from './context/MyContext';
 import Main from './componentes/Main';
 
 function App() {
 
-  const [ tarefas, setTarefas ] = useState([
-    {id: 1, title: "Fazer tela de login", done: false},
-    {id: 2, title: "Criar sistema de autenticacao na api", done: false},
-    {id: 3, title: "Criar crud com JSON", done: false}
-  ])
-
   return (
-    <MyContext.Provider value={{
-      tarefas, setTarefas
-    }}>
+    <MyContext.Provider >
       <Main />
     </MyContext.Provider>
   );
--- src/context/MyContext.js
+++ src/context/MyContext.js
@@ -1,5 +1,20 @@
-import React from 'react';
+import React, { createContext, useState } from 'react';
 
-const MyContext = React.createContext();
+export const ContextAPI = createContext();
 
-export default MyContext;
+export default function MyContext({ children }){
+
+    const [ terafas, setTarefas ] = useState([
+        {id: 1, title: "Criar sistema de login", done: false},
+        {id: 2, title: "Criat CRUD com servidor de API", done: false},
+        {id: 3, title: "Criar responsividade para dispositivos em geral", done: false}
+    ]);
+
+    return (
+        <ContextAPI.Provider value={{
+            terafas, setTarefas
+        }}>
+            {children}
+        </ContextAPI.Provider>
+    )
+}
